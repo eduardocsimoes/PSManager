44f007e8996167c81acad6caa7739bddeb2ec3e8
eduardo <eduardo@VIX-COM-006.SOLLOBRASIL.NET>
2017-03-29 07:52:05 -0300

    Merge origin/master
    Conflicts:
    	index.php

--- inc/nav.php
+++ inc/nav.php
@@ -1,24 +1,25 @@
         <nav class="menu-principal">
-            <ul class="menu clearfix">
-                <li><a href="home.php">Home</a></li>
-                <li><a href="#">Cadastro</a>
-                    <ul class="submenu-1 clearfix">
-                        <li><a href="pelada.php">Pelada</a></li>
-                        <li><a href="peladeiro.php">Peladeiro</a></li>
-                        <li><a href="posicao.php">Posicao</a></li>
-                        <li><a href="habilidade.php">Habilidade</a></li>
-                    </ul>
-                </li>    
-                <li><a href="#">Pelada</a>
-                    <ul class="submenu-1 clearfix">
-                        <li><a href="peladapeladeiro.php">Pelada x Peladeiro</a></li>                
-                        <li><a href="peladeiroposicao.php">Peladeiro x Posicao</a></li>                
-                        <li><a href="peladeirohabilidade.php">Peladeiro x Habilidade</a></li>
-                        <li><a href="peladaagendamento.php">Pelada x Agendamento</a></li>
-                        <li><a href="peladaagendamentotime.php">Pelada x Agendamento x Time</a></li>
-                    </ul>    
-                </li>    
-                <li><a href="testealgoritmo.php">Teste do Algoritmo</a></li>
+            <div class="logo">PSManager</div>
+            <div id="head-mobile"></div>
+            <div class="button"></div>
+            <ul>
+                <li class="active"><a href="home.php">Home</a></li>
+                    <li><a href="#">Cadastros</a>
+                    <li><a href="pelada.php">Pelada</a></li>
+                    <li><a href="peladeiro.php">Peladeiro</a></li>                    
+                    <li><a href="posicao.php">Posicao</a></li>
+                    <li><a href="habilidade.php">Habilidade</a></li>
+                </li>
+                <li><a href="#">Configuracao</a>
+                    <li><a href="peladapeladeiro.php">Pelada x Peladeiro</a></li>
+                    <li><a href="peladeiroposicao.php">Peladeiro x Posicao</a></li>
+                    <li><a href="peladeirohabilidade.php">Peladeiro x Habilidade</a></li>
+                    <li><a href="peladaagendamento.php">Pelada x Agendamento</a></li>
+                    <li><a href="peladaagendamentotime.php">Pelada x Agendamento x Time</a></li>                    
+                </li>
+                <li><a href="#">Distribuicao</a>
+                    <li><a href="testealgoritmo.php">Teste do Algoritmo</a></li>
+                </li>                
                 <li><a href="sair.php">Sair</a></li>
             </ul>
         </nav>   
