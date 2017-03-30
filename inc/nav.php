146aaa75f2c5460991261db7bc6bc4df1bafac6c
eduardo <eduardo@VIX-COM-006.SOLLOBRASIL.NET>
2017-03-30 16:55:36 -0300

    Merge origin/master
    Conflicts:
    	index.php

--- inc/nav.php
+++ inc/nav.php
@@ -4,21 +4,27 @@
             <div class="button"></div>
             <ul>
                 <li class="active"><a href="home.php">Home</a></li>
-                    <li><a href="#">Cadastros</a>
-                    <li><a href="pelada.php">Pelada</a></li>
-                    <li><a href="peladeiro.php">Peladeiro</a></li>                    
-                    <li><a href="posicao.php">Posicao</a></li>
-                    <li><a href="habilidade.php">Habilidade</a></li>
+                <li><a href="#">Cadastros</a>
+                    <ul>
+                        <li><a href="pelada.php">Pelada</a></li>
+                        <li><a href="peladeiro.php">Peladeiro</a></li>                    
+                        <li><a href="posicao.php">Posicao</a></li>
+                        <li><a href="habilidade.php">Habilidade</a></li>
+                    </ul>
                 </li>
                 <li><a href="#">Configuracao</a>
-                    <li><a href="peladapeladeiro.php">Pelada x Peladeiro</a></li>
-                    <li><a href="peladeiroposicao.php">Peladeiro x Posicao</a></li>
-                    <li><a href="peladeirohabilidade.php">Peladeiro x Habilidade</a></li>
-                    <li><a href="peladaagendamento.php">Pelada x Agendamento</a></li>
-                    <li><a href="peladaagendamentotime.php">Pelada x Agendamento x Time</a></li>                    
+                    <ul>
+                        <li><a href="peladapeladeiro.php">Pelada x Peladeiro</a></li>
+                        <li><a href="peladeiroposicao.php">Peladeiro x Posicao</a></li>
+                        <li><a href="peladeirohabilidade.php">Peladeiro x Habilidade</a></li>
+                        <li><a href="peladaagendamento.php">Pelada x Agendamento</a></li>
+                        <li><a href="peladaagendamentotime.php">Pelada x Agendamento x Time</a></li>    
+                    </ul>
                 </li>
                 <li><a href="#">Distribuicao</a>
-                    <li><a href="testealgoritmo.php">Teste do Algoritmo</a></li>
+                    <ul>
+                        <li><a href="testealgoritmo.php">Teste do Algoritmo</a></li>
+                    </ul>    
                 </li>                
                 <li><a href="sair.php">Sair</a></li>
             </ul>
